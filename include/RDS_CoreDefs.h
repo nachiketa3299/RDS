3abe9f702010b521c2e22758b0f2367ec893efee
Ruzen <46415457+nachiketa3299@users.noreply.github.com>
2024-02-02 00:53:59 +0900

    경로 수정

---
+++ include/RDS_CoreDefs.h
@@ -0,0 +1,12 @@
+#ifndef RDS_COREDEFS_H
+#define RDS_COREDEFS_H
+
+// clang-format off
+
+/// @short Main namespace definition
+#define RDS_BEGIN namespace rds {
+
+/// @short Main namespace undefinition
+#define RDS_END };
+
+#endif // RDS_COREDEFS_H
