a3fca108f090a06e6b627475ce7486dd2e572f38
Ruzen <46415457+nachiketa3299@users.noreply.github.com>
2024-02-02 17:00:09 +0900

    Array 템플릿 클래스 기본 작동 테스트 (확인)

--- test/Array/Array_test.cpp
+++ test/Array/Array_test.cpp
@@ -1,3 +1,5 @@
+#include <array>
+
 #include <gtest/gtest.h>
 
 #include "Array.hpp"
@@ -5,5 +7,21 @@
 using namespace rds;
 using namespace std;
 
-TEST(Array, Boundary)
-{}
+TEST(Array_test, BasicAccess)
+{
+    Array<int, 3> arr;
+    arr.operator[](0) = 0;
+    arr.operator[](1) = 1;
+    arr.operator[](2) = 2;
+
+    array<int, 3> tarr;
+    tarr.operator[](0) = 0;
+    tarr.operator[](1) = 1;
+    tarr.operator[](2) = 2;
+
+    for (auto it = arr.CBegin(); it != arr.CEnd(); ++it)
+    {
+        const auto index = it - arr.CBegin();
+        EXPECT_EQ(it.operator*(), tarr.operator[](index));
+    }
+}
