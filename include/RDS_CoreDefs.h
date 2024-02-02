800bb307134d0fc9e837518d1baab97def593a43
Ruzen <46415457+nachiketa3299@users.noreply.github.com>
2024-02-02 11:47:25 +0900

    RDS_BEGIN과 RDS_END의 주석 수정

--- include/RDS_CoreDefs.h
+++ include/RDS_CoreDefs.h
@@ -3,10 +3,10 @@
 
 // clang-format off
 
-/// @short Main namespace definition
+/// @short Main namespace( @p rds ) definition
 #define RDS_BEGIN namespace rds {
 
-/// @short Main namespace undefinition
+/// @short Main namespace( @p rds ) undefinition
 #define RDS_END };
 
 #endif // RDS_COREDEFS_H
