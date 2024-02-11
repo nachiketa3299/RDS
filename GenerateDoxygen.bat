fb712c67b7ba4d26b5c7280bb5627ef43a82ce02
Ruzen <46415457+nachiketa3299@users.noreply.github.com>
2024-02-11 20:16:29 +0900

    [feat] Doxygen 생성 후 index.html이 자동으로 열리도록 .bat 파일 수정

--- GenerateDoxygen.bat
+++ GenerateDoxygen.bat
@@ -4,3 +4,6 @@ set "DOXYGEN=E:\doxygen\bin\doxygen.exe"
 set "DOXYFILE=.\Doxyfile.config"
 
 "%DOXYGEN%" "%DOXYFILE%"
+set "INDEX_HTML=E:/OneDrive/Projects/CppProjects/RDS/docs/generated/html/index.html"
+
+start file://"%INDEX_HTML%"
