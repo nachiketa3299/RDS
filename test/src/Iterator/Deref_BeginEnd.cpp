372df1e46d95ba76636591bc560b1483497ba9ad
Ruzen <46415457+nachiketa3299@users.noreply.github.com>
2024-02-15 00:45:49 +0900

    [test] 잡다한 테스트 파일을 지움 (어차피 다시 해야 하는 거였음) 그리고 반복자를 기준으로 한 컨테이너 역참조 테스트 두 가지를 추가함

---
+++ test/src/Iterator/Deref_BeginEnd.cpp
@@ -0,0 +1,159 @@
+#include <gtest/gtest.h>
+
+#include "RDS_TestCoreDefs.h"
+
+#include "Array.hpp"
+#include "List.hpp"
+
+class N_Dereferencible_Cont_int_it_fixture: public testing::Test
+{
+
+protected:
+    rds::List<int>       list_basic{0, 1, 2};
+    const rds::List<int> clist_basic{0, 1, 2};
+
+    rds::Array<int, 3>       arr_basic{0, 1, 2};
+    const rds::Array<int, 3> carr_basic{0, 1, 2};
+};
+
+class N_Dereferencible_EmptyCont_int_it_fixture: public testing::Test
+{
+protected:
+    rds::List<int>       list_empty{};
+    const rds::List<int> clist_empty{};
+
+    rds::Array<int, 0> arr_empty{};
+    rds::Array<int, 0> carr_empty{};
+};
+
+// 리스트와 배열의 Begin 과 End 에 대해서 역참조 해보는 테스트
+// 컨테이너가 비어 있는지
+// 컨테이너가 Const 인지
+
+// NonConst Container - Not Empty - Begin
+
+TEST_F(N_Dereferencible_Cont_int_it_fixture, nc_Begin)
+{
+    EXPECT_EQ(*list_basic.Begin(), 0);
+    EXPECT_EQ(*arr_basic.Begin(), 0);
+}
+
+// Const Container - Not Empty - Begin
+
+TEST_F(N_Dereferencible_Cont_int_it_fixture, c_Begin)
+{
+    EXPECT_EQ(*clist_basic.Begin(), 0);
+    EXPECT_EQ(*carr_basic.Begin(), 0);
+}
+
+// NonConst Container - Not Empty - CBegin
+
+TEST_F(N_Dereferencible_Cont_int_it_fixture, nc_CBegin)
+{
+    EXPECT_EQ(*list_basic.CBegin(), 0);
+    EXPECT_EQ(*arr_basic.CBegin(), 0);
+}
+
+// Const Container - Not Empty - CBegin
+
+TEST_F(N_Dereferencible_Cont_int_it_fixture, c_CBegin)
+{
+    EXPECT_EQ(*clist_basic.CBegin(), 0);
+    EXPECT_EQ(*carr_basic.CBegin(), 0);
+}
+
+// NonConstContainer - Not Empty - End
+
+TEST_F(N_Dereferencible_Cont_int_it_fixture, nc_End)
+{
+    RDT_EXPECT_EXIT_FAILURE(*list_basic.End(), "");
+    RDT_EXPECT_EXIT_FAILURE(*arr_basic.End(), "");
+}
+
+// ConstContainer - Not Empty - End
+
+TEST_F(N_Dereferencible_Cont_int_it_fixture, c_End)
+{
+    RDT_EXPECT_EXIT_FAILURE(*clist_basic.End(), "");
+    RDT_EXPECT_EXIT_FAILURE(*carr_basic.End(), "");
+}
+
+// NonConstContainer - Not Empty - CEnd
+
+TEST_F(N_Dereferencible_Cont_int_it_fixture, nc_CEnd)
+{
+    RDT_EXPECT_EXIT_FAILURE(*list_basic.CEnd(), "");
+    RDT_EXPECT_EXIT_FAILURE(*arr_basic.CEnd(), "");
+}
+
+// ConstContainer - Not Empty - CEnd
+
+TEST_F(N_Dereferencible_Cont_int_it_fixture, c_CEnd)
+{
+    RDT_EXPECT_EXIT_FAILURE(*clist_basic.CEnd(), "");
+    RDT_EXPECT_EXIT_FAILURE(*carr_basic.CEnd(), "");
+}
+
+// NonConst Container - Empty - Begin
+
+TEST_F(N_Dereferencible_EmptyCont_int_it_fixture, nc_Begin)
+{
+    RDT_EXPECT_EXIT_FAILURE(*list_empty.Begin(), "");
+    RDT_EXPECT_EXIT_FAILURE(*arr_empty.Begin(), "");
+}
+
+// Const Container - Empty - Begin
+
+TEST_F(N_Dereferencible_EmptyCont_int_it_fixture, c_Begin)
+{
+    RDT_EXPECT_EXIT_FAILURE(*clist_empty.Begin(), "");
+    RDT_EXPECT_EXIT_FAILURE(*carr_empty.Begin(), "");
+}
+
+// NonConst Container - Empty - CBegin
+
+TEST_F(N_Dereferencible_EmptyCont_int_it_fixture, nc_CBegin)
+{
+    RDT_EXPECT_EXIT_FAILURE(*list_empty.CBegin(), "");
+    RDT_EXPECT_EXIT_FAILURE(*arr_empty.CBegin(), "");
+}
+
+// Const Container - Empty - CBegin
+
+TEST_F(N_Dereferencible_EmptyCont_int_it_fixture, c_CBegin)
+{
+    RDT_EXPECT_EXIT_FAILURE(*clist_empty.CBegin(), "");
+    RDT_EXPECT_EXIT_FAILURE(*carr_empty.CBegin(), "");
+}
+
+// NonConstContainer - Empty - End
+
+TEST_F(N_Dereferencible_EmptyCont_int_it_fixture, nc_End)
+{
+    RDT_EXPECT_EXIT_FAILURE(*list_empty.End(), "");
+    RDT_EXPECT_EXIT_FAILURE(*arr_empty.End(), "");
+}
+
+// ConstContainer - Empty - End
+
+TEST_F(N_Dereferencible_EmptyCont_int_it_fixture, c_End)
+{
+    RDT_EXPECT_EXIT_FAILURE(*clist_empty.End(), "");
+    RDT_EXPECT_EXIT_FAILURE(*carr_empty.End(), "");
+}
+
+// NonConstContainer - Empty - CEnd
+
+TEST_F(N_Dereferencible_EmptyCont_int_it_fixture, nc_CEnd)
+{
+    RDT_EXPECT_EXIT_FAILURE(*list_empty.CEnd(), "");
+    RDT_EXPECT_EXIT_FAILURE(*arr_empty.CEnd(), "");
+}
+
+// ConstContainer - Empty - CEnd
+
+TEST_F(N_Dereferencible_EmptyCont_int_it_fixture, c_CEnd)
+{
+    RDT_EXPECT_EXIT_FAILURE(*clist_empty.CEnd(), "");
+    RDT_EXPECT_EXIT_FAILURE(*carr_empty.CEnd(), "");
+}
---
+++ test/src/Iterator/Deref_Invalid.cpp
@@ -0,0 +1,72 @@
+#include <gtest/gtest.h>
+
+#include "RDS_TestCoreDefs.h"
+
+#include "Array.hpp"
+#include "List.hpp"
+
+TEST(Deref_Invalid, DefaultCtor_List)
+{
+    auto list_cit = rds::List<int>::ConstIterator();
+    auto list_it  = rds::List<int>::Iterator();
+
+    EXPECT_FALSE(list_cit.IsValid());
+    EXPECT_FALSE(list_it.IsValid());
+
+    EXPECT_FALSE(list_cit.IsDereferencible());
+    EXPECT_FALSE(list_it.IsDereferencible());
+
+    EXPECT_TRUE(list_it == list_cit);
+    EXPECT_TRUE(list_cit == list_it);
+
+    EXPECT_FALSE(list_cit != list_it);
+    EXPECT_FALSE(list_it != list_cit);
+
+    EXPECT_TRUE(list_cit.GetDataPointer() == nullptr);
+    EXPECT_TRUE(list_it.GetDataPointer() == nullptr);
+
+    RDT_EXPECT_EXIT_FAILURE(*list_cit, "");
+    RDT_EXPECT_EXIT_FAILURE(*list_it, "");
+}
+
+TEST(Deref_Invalid, DefaultCopyCtor_List)
+{
+    auto list_cit = rds::List<int>::ConstIterator(rds::List<int>::ConstIterator());
+    auto list_it  = rds::List<int>::Iterator(rds::List<int>::Iterator());
+
+    EXPECT_FALSE(list_cit.IsValid());
+    EXPECT_FALSE(list_it.IsValid());
+
+    EXPECT_FALSE(list_cit.IsDereferencible());
+    EXPECT_FALSE(list_it.IsDereferencible());
+
+    EXPECT_TRUE(list_it == list_cit);
+    EXPECT_TRUE(list_cit == list_it);
+
+    EXPECT_FALSE(list_cit != list_it);
+    EXPECT_FALSE(list_it != list_cit);
+
+    EXPECT_TRUE(list_cit.GetDataPointer() == nullptr);
+    EXPECT_TRUE(list_it.GetDataPointer() == nullptr);
+
+    RDT_EXPECT_EXIT_FAILURE(*list_cit, "");
+    RDT_EXPECT_EXIT_FAILURE(*list_it, "");
+}
+
+TEST(Deref_Invalid, DefaultCtor_Array_NotEmpty)
+{
+    auto arr_cit = rds::Array<int, 3>::ConstIterator();
+    auto arr_it  = rds::Array<int, 3>::Iterator();
+
+    RDT_EXPECT_EXIT_FAILURE(*arr_cit, "");
+    RDT_EXPECT_EXIT_FAILURE(*arr_it, "");
+}
+
+TEST(Deref_Invalid, DefaultCtor_Array_Empty)
+{
+    auto arr_cit = rds::Array<int, 0>::ConstIterator();
+    auto arr_it  = rds::Array<int, 0>::Iterator();
+
+    RDT_EXPECT_EXIT_FAILURE(*arr_cit, "");
+    RDT_EXPECT_EXIT_FAILURE(*arr_it, "");
+}
