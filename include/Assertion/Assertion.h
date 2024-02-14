42dd23c631191635d0bb7c98cac1459796ab8f8a
Ruzen <46415457+nachiketa3299@users.noreply.github.com>
2024-02-15 00:39:53 +0900

    [feat] RDS_Error 매크로와 내부 함수를 작성함. 또한 내부 함수가 __를 접두어로 사용하지 않도록 변경.

--- include/Assertion/Assertion.h
+++ include/Assertion/Assertion.h
@@ -1,4 +1,4 @@
-/// @file RDS_CoreDefs.h
+/// @file RDS_Assertion.h
 
 #ifndef RDS_ASSERTION_H
 #define RDS_ASSERTION_H
@@ -7,14 +7,12 @@
 
 RDS_BEGIN
 
-// clang-format off
-
 /// @brief RDS 라이브러리에서 사용하는 메인 Assertion 매크로
 /// @param[in] Expression bool 로 평가되는 표현식
-/// @see \ref __rds_assert(bool, const char*, const char*, long int*)
-#define RDS_Assert(Expression) __rds_assert(Expression, #Expression, __FILE__, __LINE__)
+/// @see \ref rds_assert(bool, const char*, const char*, long int*)
+#define RDS_Assert(Expression) rds_assert(Expression, #Expression, __FILE__, __LINE__)
 
-// clang-format on
+#define RDS_Error(Message) rds_error(Message, __FILE__, __LINE__)
 
 /// @brief Assertion 매크로 내부 함수 (직접 호출하지 말 것!)
 /// @warning 직접 호출하지 말고 #RDS_Assert 매크로를 통해 호출할 것
@@ -22,7 +20,13 @@ RDS_BEGIN
 /// @param[in] exp_str 표현식의 문자열
 /// @param[in] file_str 이 Assertion이 호출된 파일의 이름 (`__FILE__`)
 /// @param[in] line 이 Assertion이 호출된 줄 번호 (`__LINE__`)
-void __rds_assert(bool exp, const char* exp_str, const char* file_str, long int line);
+void rds_assert(bool exp, const char* exp_str, const char* file_str, long int line);
+
+/// @brief Error 매크로 내부 함수 (직접 호출하지 말 것!)
+/// @param message 에러 메시지
+/// @param file_str 에러가 발생한 파일 이름 (`__FILE__`)
+/// @param line  에러가 발생한 줄 번호 (`__LINE__`)
+void rds_error(const char* message, const char* file_str, long int line);
 
 RDS_END
 
--- src/Assertion/Assertion.cpp
+++ src/Assertion/Assertion.cpp
@@ -1,20 +1,32 @@
+/// @file Assertion.cpp
+
 #include <iostream>
 
 #include "Assertion.h"
 #include "RDS_CoreDefs.h"
 
-RDS_BEGIN
 using namespace std;
 
-void __rds_assert(bool exp, const char* exp_str, const char* file_str, long int line)
+RDS_BEGIN
+
+void rds_assert(bool exp, const char* exp_str, const char* file_str, long int line)
 {
     if (exp)
         return;
 
     cerr << "> RDS: Assertion Failed!\n";
+    cerr << "\t- Expr: " << exp_str << '\n';
     cerr << "\t- File: " << file_str << '\n';
     cerr << "\t- Line: " << line << '\n';
-    cerr << "\t- Expr: " << exp_str << '\n';
+    exit(EXIT_FAILURE);
+}
+
+void rds_error(const char* message, const char* file_str, long int line)
+{
+    cerr << "> RDS: Error!\n";
+    cerr << "\t- Msgs: " << message << '\n';
+    cerr << "\t- File: " << file_str << '\n';
+    cerr << "\t- Line: " << line << '\n';
     exit(EXIT_FAILURE);
 }
 
