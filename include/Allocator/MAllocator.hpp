958d123bf083e74595df505bc2fc9d777972e88e
Ruzen <46415457+nachiketa3299@users.noreply.github.com>
2024-02-16 17:06:57 +0900

    [feat] Mallocator 클래스 구현

--- include/Allocator/MAllocator.hpp
+++ include/Allocator/MAllocator.hpp
@@ -1,76 +1,86 @@
+/// @file Mallocator.hpp
+
 #ifndef RDS_MALLOCATOR_HPP
 #define RDS_MALLOCATOR_HPP
 
 #include <cstddef>
-#include <limits>
 #include <new>
 
+#include "RDS_Concepts.h"
 #include "RDS_CoreDefs.h"
 
 RDS_BEGIN
 
-/// @brief malloc 을 사용하는 할당자 템플릿 클래스
-/// @tparam T
-/// @details
-/// 상태 정보를 유지하지 않는(Stateless) 할당자이다. 이 말은 할당자 인스턴스 자체에서
-/// 가변적인 상태 정보를 지니지 않겠다는 의미이다. 대신 할당과 해제 과정에서 @p malloc()
-/// 과 @p free 로 명명된 전역 함수를 사용한다. 상태를 유지하지 않는 할당자는 같은 타입의
-/// 할당자와 동일한지 비교해야 한다. 이는 @ref Mallocator 인스턴스와 무관하게 @ref
-/// Mallocator 로 할당된 메모리가 반드시 @ref Mallocator 로 해제되어야 한다는 것을
-/// 의미한다. 상태를 유지하지 않는 할당자는 복잡함이 최소화된 할당자이지만, 전역 상태에
-/// 의존하기 때문에 제약이 있다.
-
-template <typename T>
+/// @brief `malloc` 과 `free`, placement `new` 를 사용하는 메모리 할당자 클래스
+/// @tparam T_t 할당할 메모리의 타입
+template <class T_t>
 class Mallocator
 {
-public:
-    using Val_t  = T;
+public: // Type Aliases
+    using Val_t  = T_t;
     using Size_t = std::size_t;
+    using Diff_t = std::ptrdiff_t; // @todo 조사
 
-public:
-    Mallocator() noexcept {}; // 기본 ctor 이 필요하지 않다!
+public: // Default CDtors
+    Mallocator()                  = default;
+    Mallocator(const Mallocator&) = default;
+    ~Mallocator()                 = default;
 
-    template <class U>
-    Mallocator(const Mallocator<U>&) const noexcept
-    {
-        return true;
-    }
+public: // Memory Allocation / Deallocation
+    auto Allocate(Size_t count) -> Val_t*;
+    auto Deallocate(Val_t* const ptr) -> void;
 
-    template <class U>
-    auto operator!=(const Mallocator<U>&) const noexcept
-    {
-        return false;
-    }
+public: // Object Construction / Destruction
+    template <class... CtorArgs_t>
+    auto Construct(Val_t* const ptr, const Size_t count, CtorArgs_t&&... CtorArgs)
+        -> void;
 
-public:
-    auto Allocate(const Size_t size) const -> Val_t*
-    {
-        if (size == 0)
-        {
-            return nullptr;
-        }
-
-        const auto max_size = std::numeric_limits<Size_t>::max() / sizeof(T);
-
-        if (size > max_size)
-        {
-            throw std::bad_array_new_length{};
-        }
-
-        void* const pv = malloc(size * sizeof(Val_t));
-
-        if (pv == nullptr)
-        {
-            throw std::bad_alloc{};
-        }
-
-        return static_cast<Val_t*>(pv);
-    }
-    auto Deallocate(Val_t* const p, Size_t) const noexcept -> void
-    {
-        free(p);
-    }
+    auto Deconstruct(Val_t* const ptr, const Size_t count) -> void;
 };
+
+RDS_END
+
+// IMPLEMENTATIONS //
+
+RDS_BEGIN
+
+template <class T_t>
+inline auto Mallocator<T_t>::Allocate(Size_t count) -> Val_t*
+{
+    // 그냥 메모리를 할당하기만 한다
+    return static_cast<Val_t*>(malloc(sizeof(Val_t) * count));
+}
+
+template <class T_t>
+inline auto Mallocator<T_t>::Deallocate(Val_t* const ptr) -> void
+{
+    // 그냥 메모리를 해제하기만 한다
+    free(ptr);
+}
+
+template <class T_t>
+template <class... CtorArgs_t>
+inline auto Mallocator<T_t>::Construct(Val_t* const ptr, const Size_t count,
+                                       CtorArgs_t&&... CtorArgs) -> void
+{
+    // 객체를 생성해야 한다
+    //::new (ptr) Val_t(std::forward<CtorArgs_t>(CtorArgs)...);
+    for (Size_t i = 0; i < count; ++i)
+    {
+        // Placement new
+        ::new (ptr + i) Val_t(std::forward<CtorArgs_t>(CtorArgs)...);
+    }
+}
+
+template <class T_t>
+inline auto Mallocator<T_t>::Deconstruct(Val_t* const ptr, const Size_t count) -> void
+{
+    // 객체를 소멸시켜야 한다
+    for (Size_t i = 0; i < count; ++i)
+    {
+        (ptr + i)->~Val_t();
+    }
+}
 
 RDS_END
 
