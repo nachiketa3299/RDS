18e5a657e4a8004bcbc138465fb2bc48fac2017c
Ruzen <46415457+nachiketa3299@users.noreply.github.com>
2024-02-16 17:31:53 +0900

    [test] Mallocatordml Fundamental Types에 대한  테스트 작성

---
+++ RDT/src/MemoryManagement/Mallocator_Fundamental_Types.cpp
@@ -0,0 +1,51 @@
+/// @file Mallocator_Fundamental_Types.cpp
+
+#include <gtest/gtest.h>
+
+#include "MAllocator.hpp"
+#include "RDT_CoreDefs.h"
+
+RDT_BEGIN
+
+template <class T_t>
+class tfMallocator_Test: public testing::Test
+{
+protected:
+    rds::Mallocator<T_t> m_mallocator{};
+};
+
+TYPED_TEST_SUITE_P(tfMallocator_Test);
+
+TYPED_TEST_P(tfMallocator_Test, Mallocate_Fundamental_Types)
+{
+    static constexpr std::size_t count = 10;
+
+    auto* ptr = this->m_mallocator.Allocate(count);
+
+    this->m_mallocator.Construct(ptr, count);
+    EXPECT_EQ(*ptr, TypeParam{});
+    this->m_mallocator.Deconstruct(ptr, count);
+
+    this->m_mallocator.Construct(ptr, count, 0);
+    EXPECT_EQ(*ptr, TypeParam{0});
+    this->m_mallocator.Deconstruct(ptr, count);
+
+    this->m_mallocator.Construct(ptr, count, 1);
+    EXPECT_EQ(*ptr, TypeParam{1});
+    this->m_mallocator.Deconstruct(ptr, count);
+
+    this->m_mallocator.Deallocate(ptr);
+}
+
+REGISTER_TYPED_TEST_SUITE_P(tfMallocator_Test, Mallocate_Fundamental_Types);
+
+INSTANTIATE_TYPED_TEST_SUITE_P(i_integers, tfMallocator_Test,
+                               cxx_fundamental_GTestTypes::integers);
+INSTANTIATE_TYPED_TEST_SUITE_P(i_booleans, tfMallocator_Test,
+                               cxx_fundamental_GTestTypes::booleans);
+INSTANTIATE_TYPED_TEST_SUITE_P(i_characters, tfMallocator_Test,
+                               cxx_fundamental_GTestTypes::characters);
+INSTANTIATE_TYPED_TEST_SUITE_P(i_floating_points, tfMallocator_Test,
+                               cxx_fundamental_GTestTypes::floating_points);
+
+RDT_END
