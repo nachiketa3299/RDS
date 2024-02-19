5c073f4d81296b42bffbb01d538b6ac0cc4e49e0
Ruzen <46415457+nachiketa3299@users.noreply.github.com>
2024-02-19 23:04:14 +0900

    [refactor] typename을 class로 변경

--- include/RDS_Concepts.h
+++ include/RDS_Concepts.h
@@ -10,7 +10,7 @@ RDS_BEGIN
 
 /// @brief 배열 원소의 형식에 대한 필요 사항: Default Constructible 해야 하며, 크기는 0
 /// 이상이어야 함.
-template <typename T_t>
+template <class T_t>
 concept ArrayRequirement = std::default_initializable<T_t>;
 
 template <class L_t, class R_t = L_t>
